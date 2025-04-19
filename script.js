e8ba5c18c8e2cb398220b8b44479c065248e0419
Ashish <ashishmasih92g@gmail.com>
2025-04-19 17:07:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+
+    let map =new Map()
+    for(let t of str.toLowerCase()){
+        
+       if(map.has(t)){
+        let v = map.get(t)
+        map.set(t,v+1)
+       }
+       else{
+        map.set(t,1)
+       }
+       
+    }
+
+  let min = 0
+  let key1 = ""
+
+for(let [key,value] of map){
+
+if(map.get(key)=="1"){
+    return key
+}
+
+}
+		return null
+	
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
